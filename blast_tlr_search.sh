8be8594b7d6b21e3c90231ed016ff1ccb19c29b8
molmagid <88408253+molmagid@users.noreply.github.com>
2021-09-21 08:15:59 +1200

    Update blast_tlr_search.sh

--- blast_tlr_search.sh
+++ blast_tlr_search.sh
@@ -1,1 +1,18 @@
+#!/bin/sh -e
 
+#make blast database for reference genome
+makeblastdb -dbtype nucl species_genome.fasta
+
+#blast each reference TLR fasta against genome database
+for file in *TLR*.fasta
+do
+base=$(basename file .fasta)
+blastn  -query $file -db species_genome.fasta -out ${base}.txt
+done
+
+#blast each reference TLR protein against genome database
+for file in *TLR_protein*.fasta
+do
+base=$(basename file .fasta)
+tblastn -query $file -db species_genome.fasta -out ${base}.txt
+done
