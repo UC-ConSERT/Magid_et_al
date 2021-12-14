6e7e2abd442ce481b6d1141015ea6ddd771ede7e
molmagid <88408253+molmagid@users.noreply.github.com>
2021-12-14 14:13:56 +1300

    Update 4_haplotype_creation.sh

--- 4_haplotype_creation.sh
+++ 4_haplotype_creation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 #phase haplotypes in filtered vcf file
-java -jar /usr/bin/beagle.28Jun21.220.jat gt=#filtered_vcf_file out=#out_vcf_file
+java -jar /usr/bin/beagle.28Jun21.220.jar gt=#filtered_vcf_file out=#out_vcf_file
 
 #
