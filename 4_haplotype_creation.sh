6fa8b6ad34a65018189c7e1f64985d68d76e493f
molmagid <88408253+molmagid@users.noreply.github.com>
2022-04-11 14:17:47 +1200

    Update 4_haplotype_creation.sh

--- 4_haplotype_creation.sh
+++ 4_haplotype_creation.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
-#phase each haplotype into a filtered vcf file
-java -jar /usr/bin/beagle.28Jun21.220.jar gt=#filtered_vcf_file out=#out_vcf_file
+#phase each haplotype into a filtered vcf file with Beagle v. 5.2
+java -jar /usr/bin/beagle.28Jun21.220.jar gt=#filtered_vcf_file out=#out_vcf_file_name
 
-#create consensus sequences with all ALT variants for comparison to ref sequence
-samtools faidx ref_genome.fa #tlr_region | bcftools consensus phased_tlr.vcf.gz -M N >> TLR_variants.fasta
+#create consensus sequences with all ALT variants in a population using samtools faidx
+#this will create a file for comparison to ref genome sequence
+samtools faidx ref_genome.fa '#tlr_region chr:start-end'| bcftools consensus phased_tlr.vcf.gz -M N >> TLR_variants.fasta
 
-#creates TLR haplotypes for all individuals and output into a MEGA file format
-counter=1
+#creates TLR haplotypes for all individuals and output sequences into a MEGA file format to use in DNAsp
+counter=1 #counter adds unique number labels to each individual for MEGA format
 echo "#MEGA" > tlr_haplotypes.meg
 #for loop to output haplotypes for each individual bam file within the final vcf for one TLR region
 for file in final_bams/*_merged.aligned.sorted.bam #loop through the bam files
